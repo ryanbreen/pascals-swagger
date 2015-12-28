92d2bfbab218e4b6210ae5d3ea2d5bd3e1cd0bb2
Ryan Breen <rbreen@cimpress.com>
2015-12-27 23:43:59 -0500

    Factor out lexer.

--- calc_mul_div.py
+++ calc_mul_div.py
@@ -27,75 +27,85 @@ class Token(object):
     def __repr__(self):
         return self.__str__()
 
+class Lexer(object):
+  def __init__(self, text):
+    # client string input, e.g. "3*5/5"
+    self.text = text
+    # self.pos is an index into self.text
+    self.pos = 0
+    # current token instance
+    self.current_token = self.get_next_token()
+
+  def error(self):
+      raise Exception('Error parsing input')
+
+  def get_next_token(self):
+      """Lexical analyzer (also known as scanner or tokenizer)
+
+      This method is responsible for breaking a sentence
+      apart into tokens. One token at a time.
+      """
+      text = self.text
+
+      # is self.pos index past the end of the self.text ?
+      # if so, then return EOF token because there is no more
+      # input left to convert into tokens
+      if self.pos > len(text) - 1:
+        return Token(EOF, None)
+
+      # get a character at the position self.pos and decide
+      # what token to create based on the single character
+      current_char = text[self.pos]
+
+      if current_char.isspace():
+        self.pos += 1
+        return self.get_next_token()
+
+      # if the character is a digit then convert it to
+      # integer, create an INTEGER token, increment self.pos
+      # index to point to the next character after the digit,
+      # and return the INTEGER token
+      if current_char.isdigit():
+        digits = current_char
+        while len(text) > self.pos+1 and text[self.pos+1].isdigit():
+          self.pos += 1
+          current_char = text[self.pos]
+          digits += current_char
+        
+        self.pos += 1
+        token = Token(INTEGER, int(digits))
+        return token
+
+      if current_char == '*' or current_char == '/':
+        token = Token(OPERATOR, current_char)
+        self.pos += 1
+        return token
+
+      self.error()
+
 
 class Interpreter(object):
-    def __init__(self, text):
-        # client string input, e.g. "3*5/5"
-        self.text = text
-        # self.pos is an index into self.text
-        self.pos = 0
-        # current token instance
-        self.current_token = self.get_next_token()
+
+    def __init__(self, lexer):
+      self.lexer = lexer
+      # set current token to the first token taken from the input
+      self.current_token = self.lexer.current_token
 
     def error(self):
-        raise Exception('Error parsing input')
-
-    def get_next_token(self):
-        """Lexical analyzer (also known as scanner or tokenizer)
-
-        This method is responsible for breaking a sentence
-        apart into tokens. One token at a time.
-        """
-        text = self.text
-
-        # is self.pos index past the end of the self.text ?
-        # if so, then return EOF token because there is no more
-        # input left to convert into tokens
-        if self.pos > len(text) - 1:
-          return Token(EOF, None)
-
-        # get a character at the position self.pos and decide
-        # what token to create based on the single character
-        current_char = text[self.pos]
-
-        if current_char.isspace():
-          self.pos += 1
-          return self.get_next_token()
-
-        # if the character is a digit then convert it to
-        # integer, create an INTEGER token, increment self.pos
-        # index to point to the next character after the digit,
-        # and return the INTEGER token
-        if current_char.isdigit():
-          digits = current_char
-          while len(text) > self.pos+1 and text[self.pos+1].isdigit():
-            self.pos += 1
-            current_char = text[self.pos]
-            digits += current_char
-          
-          self.pos += 1
-          token = Token(INTEGER, int(digits))
-          return token
-
-        if current_char == '*' or current_char == '/':
-          token = Token(OPERATOR, current_char)
-          self.pos += 1
-          return token
-
-        self.error()
+      raise Exception('Invalid syntax')
 
     def eat(self, token_type):
       # compare the current token type with the passed token
       # type and if they match then "eat" the current token
       # and assign the next token to the self.current_token,
       # otherwise raise an exception.
-      if self.current_token.type == token_type:
-        self.current_token = self.get_next_token()
+      if self.lexer.current_token.type == token_type:
+        self.lexer.current_token = self.lexer.get_next_token()
       else:
         self.error()
 
     def factor(self):
-      token = self.current_token
+      token = self.lexer.current_token
       self.eat(INTEGER)
       return token.value
 
@@ -103,8 +113,8 @@ class Interpreter(object):
       """expr -> INTEGER+ SPACE? DIVIDE|MULTIPLY SPACE? INTEGER+"""
       result = self.factor()
 
-      while self.current_token.type == OPERATOR:
-        token = self.current_token
+      while self.lexer.current_token.type == OPERATOR:
+        token = self.lexer.current_token
         self.eat(OPERATOR)
 
         right = self.factor()
@@ -126,7 +136,8 @@ def main():
         break
     if not text:
         continue
-    interpreter = Interpreter(text)
+    lexer = Lexer(text)
+    interpreter = Interpreter(lexer)
     result = interpreter.expr()
     print(result)
 
